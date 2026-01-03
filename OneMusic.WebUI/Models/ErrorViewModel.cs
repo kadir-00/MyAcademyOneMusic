d48c55a94cc517bc5f9c3d5af298556e01f7f1df
Kadir <20215802023@ogr.akdeniz.edu.tr>
2026-01-03 23:45:25 +0300

    Proje dosyalari guncellendi ve hatalar giderildi

--- OneMusic.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OneMusic.WebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
